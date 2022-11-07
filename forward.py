a50db1c9008c5972029cd28862332bd45e865c0f
Gabriel Cardoso de Faria <gacarfaria@gmail.com>
2022-11-07 14:19:53 -0300

    [IMP] Allow user messages

--- forward.py
+++ forward.py
@@ -13,12 +13,13 @@ proxy = (socks.SOCKS5, '192.111.138.29', 4145)
 client = TelegramClient('hub_noticias', api_id, api_hash)
 
 
-chats_noticias = ('Poder360', 'spcoronavirus', 'congressoemfocotelegram',
-                  'nexojornalbot', 'CNNBRASIL',  'listadapublica',
-                  'SputnikBrasil', 'g1noticias', 'ForbesBra', 'ConjurOficial',
-                  'boatosorgtl', 'bbcbrasil', 'brasil247oficial', 'noticiasuol',
-                  'SenadoNoTelegram', 'TheInterceptBR', 'estadao',
-                  'midia_ninja', 'elpaisbr', 'folha', 'vejaoficial')
+chats_noticias = ('midianinja', 'TheInterceptBR', 'SenadoNoTelegram', 'arsenaldolula', 'gacarfaria')
+#('Poder360', 'spcoronavirus', 'congressoemfocotelegram',
+#                  'nexojornalbot', 'CNNBRASIL',  'listadapublica',
+#                  'SputnikBrasil', 'g1noticias', 'ForbesBra', 'ConjurOficial',
+#                  'boatosorgtl', 'bbcbrasil', 'brasil247oficial', 'noticiasuol',
+#                  'SenadoNoTelegram', 'TheInterceptBR', 'estadao',
+#                  'midia_ninja', 'elpaisbr', 'folha', 'vejaoficial')
 chat_ids = []
 
 async def main():
@@ -27,7 +28,8 @@ async def main():
     dialogs = await client.get_dialogs()
     for name in chats_noticias:
         chat = await client.get_input_entity(name)
-        chat_ids.append(chat.channel_id)
+        #import pdb; pdb.set_trace()
+        chat_ids.append(getattr(chat, 'channel_id', False) or getattr(chat, 'user_id'))
     print(chat_ids)
 
 @client.on(events.NewMessage(chats=chat_ids, incoming=True))
@@ -35,7 +37,7 @@ async def channel_message_listener(event):
     sender = await event.get_sender()
     if sender.username in chats_noticias:
         try:
-            message = await client.forward_messages(entity='hubnoticia', messages=event.message)
+            message = await client.forward_messages(entity='HubNoticias', messages=event.message)
         except ValueError as e:
             print(event)
 
